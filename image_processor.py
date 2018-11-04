61bf001f6b352c68298bbaed51adaa2118a1b0e8
William Tarte <William.tarte6@gmail.com>
2018-11-03 23:36:09 -0400

    now it works, but need to recheck the algorithm

--- image_processor.py
+++ image_processor.py
@@ -8,6 +8,15 @@ Algorithm:
 3. Calculate constants: range of grayscale values (p), size of image
 4. Generate matrix of image
 5. Generate co occurence matrix of image (with given offset)
+    a) check image pixel at (x,y) check all pixels within offsets (left,right,top,down,diagonals)
+    b) if the grayscale intensity is within threshold, add 1 to co occurence matrix at (x,y)
+    c) when done checking, co occurence (x,y+1)
+        if at the end of the row, co occurence (x+1, y=0)
+        if index x out of bounds, break
+    d) start again starting at pixel (x, y + offset)
+        if at the end of the row or higher, image matrix (x+1, y=0)
+        if x index is at the end or out of bounds, break
+
 '''
 import cv2
 import numpy as np
@@ -26,27 +35,33 @@ p_min = np.min(img_matrix)
 
 dimension_co = p_max - p_min
 #initializing co occurence img_matrix
-co_matrix = np.zeros((dimension_co,dimension_co),dtype=int)
+co_matrix = np.zeros((dimension_co,dimension_co),dtype=np.int32)
 
 print(img)
-print(co_matrix)
-x_offset = 1
-y_offset = 1
+x_offset = 2
+y_offset = 2
 img_x = 0
 img_y = 0
 co_x = 0
 co_y = 0
 
 start_loc = [0,0]
-
+threshold = 5
 
 while True:
-    for x in range(x_offset+1):
-        for y in range(y_offset+1):
-            if img[start_loc[0]][start_loc[1]] == img[start_loc[0]+x][start_loc[1]+y]:
+    for x in range(0, x_offset):
+        for y in range(0, y_offset):
+            if abs(int(img[start_loc[0]][start_loc[1]]-img[start_loc[0]+x][start_loc[1]+y])) <= threshold:
                 co_matrix[co_x][co_y] +=1
+                continue
 
-    start_loc = [start_loc[0]+x_offset, start_loc[1]+y_offset]
+    start_loc[0] += x_offset
+    if start_loc[0] > img_matrix.shape[0]-x_offset:
+        start_loc[0] = 0
+        start_loc[1] += y_offset
+        if start_loc[1] > img_matrix.shape[1]-y_offset:
+            break
+
     co_x += 1
     if co_x >= dimension_co:
         co_x = 0
@@ -54,15 +69,16 @@ while True:
         if co_y >= dimension_co:
             break
 
-print(co_matrix)
+
+print(co_matrix[0])
 
 
 
 
 
 
-'''
+
 cv2.imshow('image',img)
+cv2.imshow('GLCM', co_matrix)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-'''
